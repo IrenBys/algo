c5d0217eba5c7c5a529bd62629e48e7bca5f6178
Irina Bystrova <irinavalentinovn@yandex.ru>
2025-07-14 00:26:59 +0300

    реализация собственного вектора

---
+++ algo/algo.cpp
@@ -0,0 +1,17 @@
+﻿// algo.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "MyVector.h"
+
+int main()
+{
+	test_size();
+	test_insert();
+	test_remove();
+	test_push_back();
+	test_pop_back();
+}
+
+
+
