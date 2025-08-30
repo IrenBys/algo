795e60f067c2c28700391677a1dad83e11bdf95a
Irina Bystrova <irinavalentinovn@yandex.ru>
2025-08-30 23:55:23 +0300

    метод цепочек для хэш такблицы

--- algo/Hash/MyHash.cpp
+++ algo/Hash/MyHash.cpp
@@ -2,73 +2,40 @@
 #include <iostream>
 #include <stdexcept> 
 
-MyHash::MyHash() {
-	for(int i = 0; i < TABLE_SIZE; i++) {
-		table[i] = 0;
-		used[i] = false;
-	}
-}
-
 void MyHash::insert(int key) {
 	int index = hashFunction(key);
 
-	for (int i = 0; i < TABLE_SIZE; i++) {		// Linear probing
-		int probeIndex = (index + i) % TABLE_SIZE;	// Wrap around the table
-		
-		// If an empty slot is found, insert the key
-		if (!used[probeIndex]) {
-			table[probeIndex] = key;
-			used[probeIndex] = true;
-			return;
-		}
-
-		if (used[probeIndex] && table[probeIndex] == key) { // Key already exists
-			return;
+	for (int val : table[index]) {	
+		if (val == key) {
+			return; // Key already exists
 		}
 	}
 
-	throw std::overflow_error("Hash table is full");
+	table[index].push_back(key);	// Insert key	
 }
 
 void MyHash::erase(int key) {
 	int index = hashFunction(key);
-	for (int i = 0; i < TABLE_SIZE; i++) {		// Linear probing
-		int probeIndex = (index + i) % TABLE_SIZE;	// Wrap around the table
-		
-		if (!used[probeIndex]) { // Key not found
-			return;
-		}
-		if (used[probeIndex] && table[probeIndex] == key) { // Key found
-			used[probeIndex] = false; // Mark as deleted
-			return;
-		}
-	}
-
-	throw std::runtime_error("Key not found");
+	table[index].remove(key); // Remove key if it exists	
 }
 
 bool MyHash::contains(int key) {
 	int index = hashFunction(key);
-	for (int i = 0; i < TABLE_SIZE; i++) {		// Linear probing
-		int probeIndex = (index + i) % TABLE_SIZE;	// Wrap around the table
-		
-		if (!used[probeIndex]) { // Key not found
-			return false;
-		}
-		if (used[probeIndex] && table[probeIndex] == key) { // Key found
-			return true;
+	for (int val : table[index]) {
+		if(val == key) {
+			return true; // Key found
 		}
 	}
 	return false;
 }
 
 void MyHash::printTable() {
-	for(int i = 0; i < TABLE_SIZE; i++) {
-		if(used[i]) {
-			std::cout << i << ": " << table[i] << std::endl;
-		} else {
-			std::cout << i << ": " << "empty" << std::endl;
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		std::cout << i << ": ";
+		for (int val : table[i]) {
+			std::cout << val << " -> ";
 		}
+		std::cout << "nullptr" << std::endl;
 	}
 }
 
